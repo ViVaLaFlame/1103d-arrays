9afc77b77c35cada4d0646799ccd087957160cbf
Amir <115877690+ViVaLaFlame@users.noreply.github.com>
2023-11-08 10:56:00 +0000

    add functions

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // задача №1
 const fill = (arraySize, value) => {
-  // решение здесь
+  let result = [];
+  for (let i = 0; i < arraySize; i += 1) {
+    result.push(value);
+  }
+  return result;
 }
 
 const data = 3;
@@ -9,16 +13,26 @@ console.log(fill(data, valueToFill))
 
 // задача №2
 const reverse = (array) => {
-  // решение здесь
- }
+  let result = [];
+  for (let i = array.length - 1; i >= 0; i -= 1) {
+    result.push(array[i]);
+  }
+  return result;
+ };
 
-const data = [1, 2, 3];
-console.log(reverse(data)); // [3, 2, 1]
+const data1= [1, 2, 3];
+console.log(reverse(data1)); // [3, 2, 1]
 
 // задача №3
 const compact = (array) => {
-  // решение здесь
-}
+  let result = [];
+  for (const value of array) {
+    if (value !== false && value !== undefined && value !== '' && value !== null && value !== 0) {
+      result.push(value);
+    }
+  }
+  return result;
+};
 
-const data = [0, 1, false, 2, undefined, '', 3, null];
-console.log(compact(data)) // [1, 2, 3]
+const data2 = [0, 1, false, 2, undefined, '', 3, null];
+console.log(compact(data2)) // [1, 2, 3]
